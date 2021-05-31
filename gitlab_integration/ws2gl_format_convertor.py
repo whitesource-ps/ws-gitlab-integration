48509b749581be6006c5d44b39dc2e135183d9bc
OzTamari <oz.tamari@whitesourcesoftware.com>
2021-05-31 13:49:29 +0300

    Switched switch name from "security" to "dependency" for GL consistency.

--- gitlab_integration/ws2gl_format_convertor.py
+++ gitlab_integration/ws2gl_format_convertor.py
@@ -2,13 +2,14 @@
 import json
 import os
 
-from ws_sdk import WS, ws_constants
+from ws_sdk import WS, ws_constants, ws_utilities
 import logging
 import sys
 
 SCHEMA_VER = "2.1"
 DEPENDENCY = "dependency"
 LICENSE = "license"
+VUL_DB_URL = "https://www.whitesourcesoftware.com/vulnerability-database"
 
 logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
 
@@ -26,10 +27,25 @@ def parse_args():
 
 
 def convert_license(conn):
+    def get_lib_locations(library_location, library):
+        locations = library_location.get('locations')
+        if len(locations):
+            if len(locations) > 1:
+                logging.warning(f"Found {len(library_location['locations'])} locations for lib {library['name']}. Using the first one")
+            loc_name = locations[0].get('path')
+        else:
+            logging.error(f"No locations found for lib {library['name']} ")
+            loc_name = None
+
+        return loc_name
+
     licenses = {}
     dependencies = []
-    libs = conn.get_licenses(full_spdx=True)
+    libs = conn.get_licenses(token=args.ws_token, full_spdx=True)
+    libs_loc = ws_utilities.convert_dict_list_to_dict(conn.get_library_location(token=args.ws_token), 'keyUuid')
+
     for lib in libs:
+        lib_loc = libs_loc[lib['keyUuid']]
         lics_lib = lib['licenses']
         curr_licenses = []
         for lic in lics_lib:
@@ -44,8 +60,8 @@ def convert_license(conn):
 
         dependencies.append({'name': lib['name'],
                              'version': lib['version'],
-                             'package_manager': lib['type'],                # TODO FIX THIS
-                             'path': "PATH",                                # TODO FIX THIS
+                             'package_manager': lib['type'],                # TODO: MAKE THIS MORE ACCURATE
+                             'path': get_lib_locations(lib_loc, lib),
                              'licenses': curr_licenses})
 
     return {'version': SCHEMA_VER,
@@ -54,7 +70,50 @@ def convert_license(conn):
 
 
 def convert_dependency(conn):
-    return {}
+    def convert_to_gl_vul(vulnerability, inventory):
+        def get_solution():
+            if vulnerability.get('topFix'):
+                ret_fix = vulnerability['topFix']['fixResolution']
+            else:
+                ret_fix = "Fix unknown"
+                logging.info(f"No fix found for {vulnerability['name']}")
+
+            return ret_fix
+
+        name = f"{vulnerability['name']}:{inventory['artifactId']}:{inventory['version']}"
+        url = f"{VUL_DB_URL}/{vulnerability['name']}"
+        gl_vul = {"category": "dependency_scanning",
+                  "name": name,
+                  "message": f"{vulnerability['name']} in {inventory['name']} - Detected by WhiteSource",
+                  "description": vulnerability['description'],
+                  "cve": vulnerability['name'],
+                  "severity": vulnerability['severity'].capitalize(),
+                  "confidence": "Confirmed",
+                  "solution": get_solution(),
+                  "scanner": {"id": "whitesource", "name": "WhiteSource"},
+                  "location": {"file": inventory['name'],
+                               "dependency": {"version": inventory['version'],
+                                              "package": {"name": inventory['artifactId']}}},
+                  "identifiers": [{"type": "whitesource",
+                                   "name": name,
+                                   "value": name,
+                                   "url": url}],
+                  "links": [{"url": url}]}
+
+        return gl_vul
+
+    vulnerabilities = conn.get_vulnerability(token=args.ws_token)
+    inventory_list = ws_utilities.convert_dict_list_to_dict(conn.get_inventory(token=args.ws_token), 'keyUuid')
+
+    gl_vuls = []
+    for vul in vulnerabilities:
+        lib_uuid = vul['library']['keyUuid']
+        gl_vul = convert_to_gl_vul(vul, inventory_list[lib_uuid])
+        gl_vuls.append(gl_vul)
+
+    return {'version': SCHEMA_VER,
+            'vulnerabilities': gl_vuls,
+            'remediations': ""}     # TODO: ADD REMEDIATION?
 
 
 if __name__ == '__main__':
